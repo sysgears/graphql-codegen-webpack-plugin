d26b4e53e3b797c16657d0401c51b1f92e9b0023
Victor Vlasenko <victor.vlasenko@sysgears.com>
2018-11-08 17:06:41 +0200

    Parse graphql docs directly

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-import { compilation, Compiler } from 'webpack';
-import { schemaToTemplateContext, makeExecutableSchema, transformDocumentsFiles, DocumentFile, FileOutput } from 'graphql-codegen-core';
-import { compileTemplate } from 'graphql-codegen-compiler';
-import { separateOperations } from 'graphql';
 import * as fs from 'fs';
+import { separateOperations } from 'graphql';
+import { compileTemplate } from 'graphql-codegen-compiler';
+import {
+  DocumentFile,
+  FileOutput,
+  makeExecutableSchema,
+  parse,
+  schemaToTemplateContext,
+  Source,
+  transformDocumentsFiles
+} from 'graphql-codegen-core';
+import mkdirp from 'mkdirp';
+import * as path from 'path';
+import { compilation, Compiler } from 'webpack';
 
 interface PluginOptions {
-  outputPath?: string;
-  template?: string
+  out?: string;
+  template?: string;
   excludeRegex?: RegExp;
   graphqlRegex?: RegExp;
 }
@@ -16,6 +26,7 @@ export default class GraphQLCodeGenPlugin {
 
   constructor(options?: PluginOptions) {
     this.options = options || {};
+    this.options.out = this.options.out || '';
     this.options.template = this.options.template || 'graphql-codegen-typescript-template';
     this.options.excludeRegex = this.options.excludeRegex || /[\\/]node_modules[\\/]/;
     this.options.graphqlRegex = this.options.graphqlRegex || /(.graphql|.gql)$/;
@@ -27,24 +38,33 @@ export default class GraphQLCodeGenPlugin {
         const typeDefs = [];
         const documents: DocumentFile[] = [];
         modules.forEach((module: any) => {
-          if (!module.error && !this.options.excludeRegex.test(module.resource) && this.options.graphqlRegex.test(module.resource)) {
+          if (
+            !module.error &&
+            !this.options.excludeRegex.test(module.resource) &&
+            this.options.graphqlRegex.test(module.resource)
+          ) {
             if (module.type !== 'javascript/auto' || !module.originalSource) {
               throw new Error('Unable to handle the module' + module);
             }
-            const ast: DocumentFile = eval(module.originalSource().source());
-            if (Object.keys(separateOperations(ast)).length !== 0) {
-              // if operations defined treat file as a document
-              documents.push({ filePath: module.resource, content: ast });
-            } else {
-              // if operations are not defined treat file as a schema part
-              typeDefs.push(ast);
+            try {
+              const ast = parse(new Source(fs.readFileSync(module.resource).toString(), module.resource));
+              if (Object.keys(separateOperations(ast)).length !== 0) {
+                // if operations defined treat file as a document
+                documents.push({ filePath: module.resource, content: ast });
+              } else {
+                // if operations are not defined treat file as a schema part
+                typeDefs.push(ast);
+              }
+            } catch (e) {
+              console.error(module.originalSource().source());
+              throw e;
             }
           }
         });
         const templateFromExport = require(this.options.template);
         const templateConfig = templateFromExport.default || templateFromExport.config || templateFromExport;
 
-        const schema = makeExecutableSchema({ typeDefs, resolvers: {}, allowUndefinedInResolve: true })
+        const schema = makeExecutableSchema({ typeDefs, resolvers: {}, allowUndefinedInResolve: true });
         const templateContext = schemaToTemplateContext(schema);
         const transformedDocs = transformDocumentsFiles(schema, documents);
         compileTemplate(templateConfig, templateContext, [transformedDocs], {
@@ -52,10 +72,12 @@ export default class GraphQLCodeGenPlugin {
           generateSchema: true
         }).then((results: FileOutput[]) => {
           results.forEach((file: FileOutput) => {
-            const content = fs.existsSync(file.filename) ? fs.readFileSync(file.filename).toString() : undefined;
+            const fullOutputPath = path.join(this.options.out, file.filename);
+            mkdirp.sync(path.dirname(fullOutputPath));
+            const content = fs.existsSync(fullOutputPath) ? fs.readFileSync(fullOutputPath).toString() : undefined;
             if (content !== file.content) {
               // TODO: add prettifier
-              fs.writeFileSync(file.filename, file.content);
+              fs.writeFileSync(fullOutputPath, file.content);
             }
           });
         });
